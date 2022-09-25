f6278d9c101537a4da83922600db68463bc402bb
969939390@qq.com <969939390@qq.com>
2022-09-25 18:16:44 +0800

    feat:111

---
+++ abouttree/102-secondtree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ *  102 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+ var levelOrder = function(root) {
+    let res = [];
+    if(root === null) return res;
+    let list = [];
+    list.push(root);//将根结点放进来
+    while(list.length) {//
+        debugger
+        let curLen = list.length;//上一轮剩下的节点，全属于下一层
+        let newLevel = [];
+        for(let i = 0; i < curLen; i++) {//同层所有节点
+            let node = list.shift();//shift拿出第一个
+            newLevel.push(node.val);//push进newLevel数组
+            //左右子节点push进队列  将下一层的节点放心去
+            if(node.left) list.push(node.left);
+            if(node.right) list.push(node.right);
+        }
+        res.push(newLevel);//push到res数组
+    }
+    return res;
+};
