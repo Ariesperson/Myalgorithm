e5ee6d54142cb8109ad9f9ebc26ac3e60ebb3e27
siyan.chen <siyan.chen@dmall.com>
2022-10-27 17:43:14 +0800

    feat:27日Leetcode提交

--- aboutarr/200-islandnum.js
+++ aboutarr/200-islandnum.js
@@ -3,3 +3,50 @@
 // 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
 
 // 此外，你可以假设该网格的四条边均被水包围。
+
+// 示例 1：
+
+// 输入：grid = [
+//   ["1","1","1","1","0"],
+//   ["1","1","0","1","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","0","0","0"]
+// ]
+// 输出：1
+
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+ const numIslands = (grid) => {
+   let count = 0
+   for (let i = 0; i < grid.length; i++) {
+       for (let j = 0; j < grid[0].length; j++) {//循环网格
+           if (grid[i][j] === '1') {//如果为陆地，count++，
+               count++
+               console.log(count)
+               turnZero(i, j, grid)
+           }
+       }
+   }
+   return count
+}
+function turnZero(i, j, grid) {//沉没四周的陆地
+   if (i < 0 || i >= grid.length || j < 0
+       || j >= grid[0].length || grid[i][j] === '0') return //检查坐标的合法性
+   grid[i][j] = '0'//让四周的陆地变为海水
+   turnZero(i, j + 1, grid)
+   turnZero(i, j - 1, grid)
+   turnZero(i + 1, j, grid)
+   turnZero(i - 1, j, grid)
+}
+
+
+ let result = numIslands([
+    ["1","1","1","1","0"],
+    ["1","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"]
+  ])
+  
