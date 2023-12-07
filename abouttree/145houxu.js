fc50df818f276d03758303729372fedc4db1241f
siyansychen <siyansychen@tencent.com>
2023-12-07 16:07:32 +0800

    feat: 二叉树后序遍历

--- abouttree/145houxu.js
+++ abouttree/145houxu.js
@@ -10,8 +10,8 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-//二叉树的中序遍历：按照访问左子树——根节点——右子树的方式遍历这棵树，而在访问左子树或者右子树的时候我们按照同样的方式遍历，直到遍历完整棵树。因此整个遍历过程天然具有递归的性质，我们可以直接用递归函数来模拟这一过程。
-// 即递归逻辑上优先处理left节点,然后是root.val,然后是right
+//二叉树的后序遍历：按照访问左子树——右子树——根节点的方式遍历这棵树，而在访问左子树或者右子树的时候我们按照同样的方式遍历，直到遍历完整棵树。因此整个遍历过程天然具有递归的性质，我们可以直接用递归函数来模拟这一过程。
+// 即递归逻辑上优先处理left节点,right 然后是root.val
 
 var inorderTraversal = function(root) {
     let res = []
@@ -20,8 +20,8 @@ var inorderTraversal = function(root) {
             return;
         }
         orderdeep(root.left)
-        res.push(root.val)
         orderdeep(root.right)
+        res.push(root.val)
     }
     orderdeep(root)
     return res
