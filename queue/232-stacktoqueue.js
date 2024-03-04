439f700e7e91a43f224242e8f399e0d07cdf4501
siyansychen <969939390@qq.com>
2024-03-04 16:45:06 +0800

    feat: 日常练习栈转队列

---
+++ queue/232-stacktoqueue.js
@@ -0,0 +1,41 @@
+var MyQueue = function() {
+    this.inStack = [];
+    this.outStack = [];
+};
+
+MyQueue.prototype.push = function(x) {
+    this.inStack.push(x);
+};
+
+MyQueue.prototype.pop = function() {
+    if (!this.outStack.length) {
+        this.in2out();
+    }
+    return this.outStack.pop();
+};
+
+MyQueue.prototype.peek = function() {
+    if (!this.outStack.length) {
+        this.in2out();
+    }
+    return this.outStack[this.outStack.length - 1];
+};
+
+MyQueue.prototype.empty = function() {
+    return this.outStack.length === 0 && this.inStack.length === 0;
+};
+
+MyQueue.prototype.in2out = function() {
+    while (this.inStack.length) {
+        this.outStack.push(this.inStack.pop());
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
